d6a4d5da27d7b70a0475d0ed1519eb7544061a8c
Akansha <ft.akansha@gmail.com>
2026-02-24 15:22:09 +0530

    Deploy fixed build with relative paths to gh-pages branch

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/UndoSchool/',
+    base: './',
     build: {
-        outDir: 'docs',
-    },
-    server: {
-        open: '/dev.html',
+        outDir: 'dist',
     },
 })
